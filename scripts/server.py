5641a48b4444b8d3d915927afc5ac68b05887cc4
root <root@paula.es.una.ninja>
2015-11-30 17:13:18 +0100

    random server almost ready

--- scripts/server.py
+++ scripts/server.py
@@ -2,6 +2,7 @@ from BaseHTTPServer import BaseHTTPRequestHandler
 import urlparse
 import MySQLdb
 from time import sleep
+from time import gmtime, strftime
 import os
 import subprocess
 import digitalocean
@@ -46,18 +47,21 @@ zoneNames = [
     'can'
 ]
 
-db_host = 'kafir.es'
-user = 'vpn'
+db_host = 'localhost'
+db_user = 'vpn'
 password = 'XLRc3H1y4Db0G4qR630Qk2xPF3D88P'
 database = 'vpn'
-db = MySQLdb.connect(host=db_host, user=user, passwd=password, db=database)
+db = None
+#db = MySQLdb.connect(host=db_host, user=user, passwd=password, db=database)
 
 def getUsername(userId):
+    db = MySQLdb.connect(host=db_host, user=db_user, passwd=password, db=database)
     cursor = db.cursor()
     query = ("SELECT username FROM users WHERE id={};".format(userId))
     cursor.execute(query)
     data = cursor.fetchone()
     cursor.close()
+    db.close()
     # userId and zone
     return data[0]
 
@@ -89,11 +93,13 @@ def pointIP(url, ip):
 
 
 def getInfoFromToken(token):
+    db = MySQLdb.connect(host=db_host, user=db_user, passwd=password, db=database)
     cursor = db.cursor()
     query = ("SELECT user_id, zone FROM servers WHERE token='{}';".format(token))
     cursor.execute(query)
     serverToCreate = cursor.fetchone()
     cursor.close()
+    db.close()
     # machineId and zone
     if serverToCreate:
         return serverToCreate[0], serverToCreate[1]
@@ -101,7 +107,7 @@ def getInfoFromToken(token):
         return None, None
 
 
-def createDropletDigitalOcean(url, region):
+def createDropletDigitalOcean(url, region, point=True):
     manager = digitalocean.Manager(token=APIToken)
     droplet = digitalocean.Droplet(token=APIToken,
                                    name=url,
@@ -117,13 +123,15 @@ def createDropletDigitalOcean(url, region):
     sleep(10)
     dropletData = manager.get_droplet(dropletID)
     ip = dropletData.ip_address
-    pointIP(url, ip)
+    if(point):
+         pointIP(url, ip)
     sleep(30)
     # ip = dropletData.ip_address
     return ip,dropletID
 
 
 def setUpVPNServer(userId,token,url,dropletID,ip):
+    db = MySQLdb.connect(host=db_host, user=db_user, passwd=password, db=database)
     cursor = db.cursor()
     query = ("SELECT name, password FROM vpnusers WHERE user_id={};".format(userId))
     cursor.execute(query)
@@ -133,24 +141,28 @@ def setUpVPNServer(userId,token,url,dropletID,ip):
     cursor.execute(update_query)
     db.commit()
     cursor.close()
-
-    userfile = open("../ansible/files/secrets/"+url,'w')
+    db.close()
+    ansible_token = url + strftime("%Y-%m-%d_%H:%M:%S", gmtime())
+    userfile = open("../ansible/files/secrets/"+ansible_token,'w')
     index=210
     for user in users:
         userfile.write(user[0]+'\t*\t'+user[1]+'\t*\t192.168.155.'+str(index)+'\n')
         index += 1
     userfile.close()
 
-    inventoryfile = open("../ansible/inventories/"+url,'w')
+    inventoryfile = open("../ansible/inventories/"+ansible_token,'w')
     inventoryfile.write('[target]\n{}\n'.format(ip))
     inventoryfile.close()
 
     print "Lanzamos script de instalacion (ansible)"
-    subprocess.check_call('/usr/bin/ansible-playbook ../ansible/pptp_setup.yml -i ../ansible/inventories/{} -e "secrets_file={}"'.format(url,url), shell=True)
-    
+    try:
+         subprocess.check_call('/usr/bin/ansible-playbook ../ansible/pptp_setup.yml -i ../ansible/inventories/{} -e "secrets_file={}"'.format(ansible_token,ansible_token), shell=True)
+    except:
+         setUpVPNServer(userId,token,url,dropletID,ip)
 
 
 def createDroplet(zone, userId, token):
+    sleep(5)
     zoneinfo = zones[zone]
     candidateProviders = []
     for provider in providers:
@@ -161,11 +173,13 @@ def createDroplet(zone, userId, token):
     region = regions[random.randint(0, len(regions) - 1)]
     username = getUsername(userId)
     url = zoneNames[zone] + '.' + username + '.vpn.windmaker.net'
+    db = MySQLdb.connect(host=db_host, user=db_user, passwd=password, db=database)
     cursor = db.cursor()
     query = ("SELECT name FROM servers WHERE user_id={};".format(userId))
     cursor.execute(query)
     results = cursor.fetchall()
     cursor = db.cursor()
+    db.close()
     usedURLS = []
     for r in results:
          usedURLS.append(r[0])
@@ -177,6 +191,7 @@ def createDroplet(zone, userId, token):
     
     print url
     print region
+    db = MySQLdb.connect(host=db_host, user=db_user, passwd=password, db=database)
     cursor = db.cursor()
     update_query = ("UPDATE servers SET name='{}' , true_zone='{}' , provider='{}' , status='Setting up...'  WHERE token='{}';".format(url, region, provider, token))
     cursor.execute(update_query)
@@ -191,21 +206,24 @@ def createDroplet(zone, userId, token):
     cursor.execute(update_query)
     db.commit()
     cursor.close()
-
+    db.close()
     response = os.system("ping -c 1 " + ip)
     while response != 0:
         sleep(0.5)
         response = os.system("ping -c 1 " + ip)
-    sleep(10)
+    sleep(15)
     print "Set up config"
     setUpVPNServer(userId,token,url,dropletID,ip)
+    db = MySQLdb.connect(host=db_host, user=db_user, passwd=password, db=database)
     cursor = db.cursor()
     update_query = ("UPDATE servers SET status='Running'  WHERE token='{}';".format(token))
     cursor.execute(update_query)
     db.commit()
     cursor.close()
+    db.close()
 
 def poweroff(token):
+    db = MySQLdb.connect(host=db_host, user=db_user, passwd=password, db=database)
     cursor = db.cursor()
     query = ("SELECT machine_id , provider, ip FROM servers WHERE token='{}';".format(token))
     cursor.execute(query)
@@ -233,13 +251,15 @@ def poweroff(token):
     cursor.execute(update_query)
     db.commit()
     cursor.close()
+    db.close()
 
 def poweron(token):
+    db = MySQLdb.connect(host=db_host, user=db_user, passwd=password, db=database)
     cursor = db.cursor()
     query = ("SELECT machine_id , provider , ip FROM servers WHERE token='{}';".format(token))
     cursor.execute(query)
     results = cursor.fetchall()[0]
-    cursor = db.cursor()
+    cursor.close() 
     print results
     #cursor = db.cursor()
     #update_query = ("UPDATE servers SET status='Powering on'  WHERE token='{}';".format(token))
@@ -264,8 +284,10 @@ def poweron(token):
     cursor.execute(update_query)
     db.commit()   
     cursor.close()
+    db.close()
 
 def destroy(token):
+    db = MySQLdb.connect(host=db_host, user=db_user, passwd=password, db=database)
     cursor = db.cursor()
     query = ("SELECT machine_id , provider , ip FROM servers WHERE token='{}';".format(token))
     cursor.execute(query)
@@ -283,6 +305,63 @@ def destroy(token):
               if str(droplet).split(' ')[0] == str(results[0]):
                    print "Lo borro"
                    droplet.destroy()
+    db.close()
+
+def createrandomserver(zone, userId, token):
+    db = MySQLdb.connect(host=db_host, user=db_user, passwd=password, db=database)
+    zoneinfo = zones[zone]
+    candidateProviders = []
+    for provider in providers:
+        if provider in zoneinfo:
+            candidateProviders.append(provider)
+    provider = candidateProviders[random.randint(0, len(candidateProviders) - 1)]
+    regions = zoneinfo[provider]
+    region = regions[random.randint(0, len(regions) - 1)]
+    username = getUsername(userId)
+    url = 'random.' + username + '.vpn.windmaker.net'
+    cursor = db.cursor()
+    query = ("SELECT name FROM servers WHERE user_id={};".format(userId))
+    cursor.execute(query)
+    results = cursor.fetchall()
+    cursor.close()
+    print url
+    print region
+    cursor = db.cursor()
+    update_query = ("UPDATE servers SET name='{}' , true_zone='{}' , provider='{}' , status='Setting up...'  WHERE token='{}';".format(url, region, provider, token))
+    cursor.execute(update_query)
+    db.commit()
+    cursor.close()
+    db.close()
+    if provider == 'DigitalOcean':
+        ip , dropletID = createDropletDigitalOcean(url, region,False)
+        print ip
+    db = MySQLdb.connect(host=db_host, user=db_user, passwd=password, db=database)
+    cursor = db.cursor()
+    update_query = ("UPDATE servers SET machine_id={} , ip='{}' WHERE token='{}';".format(dropletID, ip, token))
+    cursor.execute(update_query)
+    db.commit()
+    cursor.close()
+    db.close()
+    response = os.system("ping -c 1 " + ip)
+    while response != 0:
+        sleep(0.5)
+        response = os.system("ping -c 1 " + ip)
+    sleep(20)
+    print "Set up config"
+    setUpVPNServer(userId,token,url,dropletID,ip)
+    db = MySQLdb.connect(host=db_host, user=db_user, passwd=password, db=database)
+    cursor = db.cursor()
+    update_query = ("UPDATE servers SET status='Running'  WHERE token='{}';".format(token))
+    cursor.execute(update_query)
+    db.commit()
+    cursor.close()
+    db.close()
+    poweroff(token)
+    response = os.system("ping -c 1 " + ip)
+    while response == 0:
+        sleep(1)
+        poweroff(token)
+    
 
 class theThread(threading.Thread):
     def __init__(self, action, zone=None, userId=None, token=None):
@@ -302,13 +381,52 @@ class theThread(threading.Thread):
               poweron(self.token)
          elif self.action == 'destroy':
               destroy(self.token)
-
+         elif self.action == 'createrandomserver':
+              db = MySQLdb.connect(host=db_host, user=db_user, passwd=password, db=database)
+              cursor = db.cursor()
+              query = ("SELECT user_id FROM servers WHERE token='{}';".format(self.token))
+              cursor.execute(query)
+              results = cursor.fetchall()[0]
+              user_id = int(results[0]);
+              cursor.close()
+              #cursor = db.cursor()
+              #query = ("SELECT token FROM servers WHERE user_id={};".format(user_id))
+              #cursor.execute(query)
+              #results = cursor.fetchall()
+              #print results
+              #cursor.close()
+              createrandomserver(self.zone, self.userId, self.token)
+              db.close()
+              #counter = 0
+              #for token in results:
+              #     print token[0]
+              #     userId, zone = getInfoFromToken(token[0])
+              #     createrandomserver(int(zone), userId, token[0])
+              #     counter += 1
+              #     cursor = db.cursor()
+              #     update_query = ("UPDATE randomservers SET number={}  WHERE user_id={};".format(counter,user_id))
+              #     cursor.execute(update_query)
+              #     db.commit()
+         elif self.action == 'enablerandomserver':
+              db = MySQLdb.connect(host=db_host, user=db_user, passwd=password, db=database)
+	      cursor = db.cursor()
+              query = ("SELECT ip,name FROM servers WHERE token='{}';".format(self.token))
+              cursor.execute(query)
+              results = cursor.fetchall()[0]
+              cursor.close()
+              db.close()
+              ip = results[0] 
+              url = results[1]
+              print ip
+              print url
+              pointIP(url, ip)
+              poweron(self.token)              
 
 class GetHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         parsed_path = urlparse.urlparse(self.path)
         message_parts = []
-        if parsed_path.path == '/createserver' or parsed_path.path == '/poweroff' or parsed_path.path == '/poweron' or parsed_path.path == '/destroy':
+        if parsed_path.path == '/createserver' or parsed_path.path == '/poweroff' or parsed_path.path == '/poweron' or parsed_path.path == '/destroy' or parsed_path.path == '/createrandomserver' or parsed_path.path == '/enablerandomserver':
             arguments = dict()
             print(self.client_address)
             args = parsed_path.query.split('&')
@@ -330,6 +448,10 @@ class GetHandler(BaseHTTPRequestHandler):
                         t = theThread('poweron', int(zone), userId, arguments['token'])
                     elif parsed_path.path == '/destroy':
                         t = theThread('destroy', int(zone), userId, arguments['token'])
+                    elif parsed_path.path == '/createrandomserver':
+                        t = theThread('createrandomserver', int(zone), userId, arguments['token'])
+                    elif parsed_path.path == '/enablerandomserver':
+                        t = theThread('enablerandomserver', int(zone), userId, arguments['token'])
                     t.start()  
                     #createDroplet(int(zone), userId, arguments['token'])
         for name, value in sorted(self.headers.items()):
